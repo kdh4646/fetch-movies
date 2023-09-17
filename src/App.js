75758ec5553c10bb8321a7675dfcf6fea2decbe0
Derek Kweon <kdh4646@hanmail.net>
2023-09-16 23:16:26 -0700

    2023-09-16 then() & async, await

--- src/App.js
+++ src/App.js
@@ -30,6 +30,23 @@ function App() {
       });
   }
 
+  //Another way rather than using then() => async, await
+  // async function fetchMoviesHandler() {
+  //   //returns promise
+  //   const response = await fetch("https://swapi.dev/api/films/");
+  //   const data = await response.json();
+  //   const transformedMovies = data.results.map((movieData) => {
+  //     return {
+  //       id: movieData.episode_id,
+  //       title: movieData.title,
+  //       openingText: movieData.opening_crawl,
+  //       releaseDate: movieData.release_date,
+  //     };
+  //   });
+
+  //   setMovies(transformedMovies);
+  // }
+
   return (
     <React.Fragment>
       <section>
